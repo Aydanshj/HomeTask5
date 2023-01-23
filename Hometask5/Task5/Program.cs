776650b35177f8630cf3637c35f4c44250e93477
Aydanshj <121218604+Aydanshj@users.noreply.github.com>
2023-01-24 00:00:55 +0400

    Add files via upload

---
+++ Hometask5/Task5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace HomeTask5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 3, 5, 7, 9, 13 };
+            int num = 5;
+            AddNewNumToArray(ref arr, newNum);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            static void AddNewNumToArray(ref int[] arr,int num)
+            {
+                int[] new Array = new int[arr.Length + 1];
+                int j = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    newArray[j] = arr[i];
+                    j++;
+                    
+                }
+                newArray[newArray.Length - 1] = newNum;
+                arr = newArray;
+            }
+        }
+
+    }
+}
